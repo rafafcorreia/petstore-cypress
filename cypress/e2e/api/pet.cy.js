20b0118363d6f2711a87d0b92c850f5d99d2f886
rafafcorreia <rafa.f.correia@gmail.com>
2024-08-15 20:41:57 -0300

    Testes DDT

--- cypress/e2e/api/pet.cy.js
+++ cypress/e2e/api/pet.cy.js
@@ -1,5 +1,6 @@
 import pet from '../../fixtures/pet.json'
 import petNovo from '../../fixtures/petNovo.json'
+import listaPets from '../../fixtures/listaPets.json'
 
 describe("CRUD Pet", () => {
 
@@ -60,5 +61,35 @@ describe("CRUD Pet", () => {
             expect(body.type).to.eq('unknown')
             expect(body.message).to.eq(`${pet.id}`)
         })
+    });
+
+    ;listaPets.forEach((element, index) => {
+        it(`POST Pet DDT - ${element.name}`, () => {
+            cy.request({
+                method: 'POST',
+                url: '/pet',
+                body: listaPets[index]
+            }).then(({ status, body }) => {
+                expect(status).to.eq(200)
+                expect(body.id).to.eq(listaPets[index].id)
+                expect(body.category.name).to.eq(listaPets[index].category.name)
+                expect(body.name).to.eq(listaPets[index].name)
+                expect(body.tags[0].id).to.eq(listaPets[index].tags[0].id)
+                expect(body.tags[0].name).to.eq(listaPets[index].tags[0].name)
+                expect(body.status).to.eq(listaPets[index].status)
+            })
+        });
+
+        it(`DELETE Pet DDT - ${element.name}`, () => {
+            cy.request({
+                method: 'DELETE',
+                url: `/pet/${listaPets[index].id}`
+            }).then(({status, body}) => {
+                expect(status).to.eq(200)
+                expect(body.code).to.eq(200)
+                expect(body.type).to.eq('unknown')
+                expect(body.message).to.eq(`${listaPets[index].id}`)
+            })
+        });
     });
 });
